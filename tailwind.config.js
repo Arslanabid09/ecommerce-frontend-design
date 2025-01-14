81287f3850ac3200846d4584113ae1a11f9c831f
Arslan Abid <arslancode71@gmail.com>
2025-01-14 19:02:26 +0500

    header and footer completed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        customBlue:"#0D6EFD"
+      },
+      fontFamily:{
+        inter:'"Inter", "serif"'
+      }
+    },
+  },
+  plugins: [],
+}
+
